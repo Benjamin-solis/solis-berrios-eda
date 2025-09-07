bbbd4c55994a5c2c2a441f2e557da6aa0f0ffc21
pbn <AndresBerriosL@miuandes.cl>
2025-09-07 20:22:47 -0300

    .h corregido

--- clustering/cpp/include/simsearch.hpp
+++ clustering/cpp/include/simsearch.hpp
@@ -1,14 +1,21 @@
 #ifndef _SIM_SEARCH_HPP
 #define _SIM_SEARCH_HPP
 
+#include <cstddef>
+
+class Matrix;
+
 class SimSearch{
 private:
     const Matrix &mat_data; // data to be processed
     const Matrix &mat_clusters; // centroids to be used
 
 public:
-    SimSearch(Matrix, clusters);
-    std::vector<size_t> search_with_clusters(const float *query,  top_k);
+    SimSearch(const Matrix& data, const Matrix& centroids);
+
+    std::vector<std::size_t>
+    search_with_clusters(const float* query, std::size_t top_k) const;
+
     std::vector<size_t> search_without(const float *query, top_k);
 }
 
