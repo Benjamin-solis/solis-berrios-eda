b547aa77595d096f19a5792b5932d9a8664d9ecb
pbn <AndresBerriosL@miuandes.cl>
2025-09-07 21:36:47 -0300

    search_without listo en teoria

--- clustering/cpp/src/simsearch.cpp
+++ clustering/cpp/src/simsearch.cpp
@@ -3,11 +3,11 @@
 #include "../include/utils.hpp" //Para usar argsort()
 
 
-std::size_t dis(const std::size_t * a, std::size_t * b){
-    std::size_t dx = a[0] - b[0];
-    std::size_t dy = a[1] - b[1];
+static inline float dis(const float * a, const float * b){
+    float dx = a[0] - b[0];
+    float dy = a[1] - b[1];
 
-    return dx*dx - dy*dy;
+    return dx*dx + dy*dy;
 }
 
 SimSearch::SimSearch(const Matrix &data, const Matrix &clusters)
@@ -22,5 +22,11 @@ std::vector<std::size_t> SimSearch::search_without(const float* query, std::size
         const float* row = mat_data.getRow(i);
         dist[i] = dis(query,  row);
     }
-    return {};
-    }
+
+    std::size_t *indice = argsort(dist.data(), mat_data.getN());
+
+
+    std::vector<size_t> fin(indice, indice + top_k);
+    free(indice);
+    return fin;
+}
